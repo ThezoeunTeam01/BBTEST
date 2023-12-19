f0f571b7051d644c7269b63915bdc9196922da3b
ByeungJi <dprmakak@gmail.com>
2023-12-19 17:33:21 +0900

    BBTEST

---
+++ src/main/java/com/example/bbtest/BbtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bbtest/BbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
